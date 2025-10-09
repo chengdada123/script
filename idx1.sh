cfa237686f9cfa8e10aee6c02a2f340ae909f235
chengdada123 <59160585+chengdada123@users.noreply.github.com>
2025-10-10 02:42:02 +0800

    Update idx1.sh

--- idx1.sh
+++ idx1.sh
@@ -15,6 +15,7 @@ printf '3\n1\n8\n\n\n\n\n200G\n10240\n8\n\n\n\n\n2\n1\n' | bash ./main.sh &
 read -p "按回车键继续执行后续命令..."  # 等你手动回车
 
 printf '3\n3\n1\n' | bash ./main.sh
+read -p "按回车键继续执行后续命令..."  # 等你手动回车
 wget -O vm.sh https://raw.githubusercontent.com/chengdada123/script/refs/heads/main/vm.sh
 chmod +x vm.sh
 cd ~/vms
