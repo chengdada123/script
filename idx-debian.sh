8ecbc7b03b1dcc290cb41950e026d17f19fdbaab
chengdada123 <59160585+chengdada123@users.noreply.github.com>
2025-10-10 10:18:01 +0800

    Update idx-debian.sh

--- idx-debian.sh
+++ idx-debian.sh
@@ -10,7 +10,17 @@ chmod +x main.sh
 echo "清理"
 printf '3\n6\n1\ny\n\0\n' | bash ./main.sh &
 read -p "按回车键继续执行后续命令..."  # 等你手动回车
-echo "创建debian虚拟机，完成后请按回车键"
-printf '3\n1\n8\nwinsrv2022\ndebian\nroot\nvps888\n200G\n10240\n8\n\n\n\n\n2\n1\n' | bash ./main.sh &
-read -p "按回车键继续执行后续命令..."  # 等你手动回车
-echo "创建debian虚拟机成功，用户名：root 密码：vps888 你可以在当前页面通过 ssh -p 2222 root@localhost 登录"
+echo "创建debian虚拟机，用户名：root 密码：vps888 "
+
+read -p "是否继续？(y/n): " choice
+if [ "$choice" = "y" ] || [ "$choice" = "Y" ]; then
+    echo "继续执行..."
+    # 在这里写要执行的命令
+else
+    echo "已取消。"
+    exit 1
+fi
+
+printf '3\n1\n8\nwinsrv2022\ndebian\nroot\nvps888\n200G\n10240\n8\n\n\n\n\n2\n1\n' | bash ./main.sh 
+
+
