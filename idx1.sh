3e8c877fdc8d98d22c7e1c74d8b7065f339b83ba
chengdada123 <59160585+chengdada123@users.noreply.github.com>
2025-10-10 09:07:58 +0800

    Update idx1.sh

--- idx1.sh
+++ idx1.sh
@@ -7,7 +7,7 @@ set -o pipefail
 cd ~/vps
 wget -O vm.sh https://raw.githubusercontent.com/chengdada123/script/refs/heads/main/vm.sh
 chmod +x vm.sh
-printf '1\n8\n\n\n\n\n200G\n10240\n8\n\n\n\n\n0\n' | bash ./vm.sh &
+printf '1\n8\nwinsrv2022\n\n\n\n200G\n10240\n8\n\n\n\n\n0\n' | bash ./vm.sh &
 
 
 #read -p "按回车键继续执行后续命令..."  # 等你手动回车
