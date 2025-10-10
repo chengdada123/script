f4feeb6263a8501bdfbb2c4e52adbd0603a067d2
chengdada123 <59160585+chengdada123@users.noreply.github.com>
2025-10-10 09:15:08 +0800

    Update idx1.sh

--- idx1.sh
+++ idx1.sh
@@ -7,6 +7,8 @@ set -o pipefail
 cd ~/vps
 wget -O vm.sh https://raw.githubusercontent.com/chengdada123/script/refs/heads/main/vm.sh
 chmod +x vm.sh
+
+echo "创建debian虚拟机，完成后请按回车键"
 printf '1\n8\nwinsrv2022\n\n\n\n200G\n10240\n8\n\n\n\n\n0\n' | bash ./vm.sh &
 
 
@@ -25,5 +27,7 @@ wget -O winsrv2022.qcow2 https://ip.nl8.eu/winsrv2022.qcow2
 
 echo "启动虚拟机"
 printf '2\n1\n' | bash ./vm.sh &
+
+echo "完成！监听端口3389，请使用内网穿透工具链接，密码请联系TG @chushuo_ge"
 
  
