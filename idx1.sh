ed7ae829eacc13950956bce2e640581a599b9882
chengdada123 <59160585+chengdada123@users.noreply.github.com>
2025-10-10 08:14:25 +0800

    Update idx1.sh

--- idx1.sh
+++ idx1.sh
@@ -15,6 +15,11 @@ printf '3\n1\n8\n\n\n\n\n200G\n10240\n8\n\n\n\n\n2\n1\n' | bash ./main.sh &
 read -p "按回车键继续执行后续命令..."  # 等你手动回车
 
 printf '3\n3\n1\n' | bash ./main.sh
+
+conf_file=$(ls *.conf 2>/dev/null)
+if [ -n "$conf_file" ]; then
+    sed -i 's|^IMG_URL=.*|IMG_URL="'"$FINAL_QCOW2"'"|' "$conf_file"
+fi
 read -p "按回车键继续执行后续命令..."  # 等你手动回车
 wget -O vm.sh https://raw.githubusercontent.com/chengdada123/script/refs/heads/main/vm.sh
 chmod +x vm.sh
@@ -24,10 +29,7 @@ wget https://ip.nl8.eu/winsrv2022.qcow2
 
 echo "配置 vm.sh..."
 
-conf_file=$(ls *.conf 2>/dev/null)
-if [ -n "$conf_file" ]; then
-    sed -i 's|^IMG_URL=.*|IMG_URL="'"$FINAL_QCOW2"'"|' "$conf_file"
-fi
+
 
 
 
