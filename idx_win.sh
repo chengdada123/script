413290c303356d458cc5678e6c419f61b45f7a54
chengdada123 <59160585+chengdada123@users.noreply.github.com>
2025-10-10 10:44:01 +0800

    update

--- idx_win.sh
+++ idx_win.sh
@@ -7,16 +7,21 @@ set -o pipefail
 cd ~/vps
 wget -O vm.sh https://raw.githubusercontent.com/chengdada123/script/refs/heads/main/vm.sh
 chmod +x vm.sh
-echo "清理"
-printf '6\n1\ny\n\0\n' | bash ./vm.sh &
+read -p "之前是否安装过虚拟机？(y/n): " choice
+if [ "$choice" = "y" ] || [ "$choice" = "Y" ]; then
+    echo "继续执行..."
+    printf '6\n1\ny\n\0\n' | bash ./vm.sh &
+else
+
+
+
+
 
 echo "创建debian虚拟机，完成后请按回车键"
 printf '1\n8\nwinsrv2022\n\n\n\n200G\n10240\n8\n\n\n\n\n0\n' | bash ./vm.sh &
 
 
-#read -p "按回车键继续执行后续命令..."  # 等你手动回车
 
-#printf '3\n1\n' | bash ./vm.sh &
 
 
 read -p "按回车键继续执行后续命令..."  # 等你手动回车
@@ -35,5 +40,6 @@ cd ~/vps
 printf '2\n1\n' | bash ./vm.sh &
 
 echo "完成！监听端口3389，请使用内网穿透工具链接，密码请联系TG @chushuo_ge"
+fi
 
  
