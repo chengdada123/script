39727d9b72e83a740da31d7f2005ebe413d79dd8
chengdada123 <59160585+chengdada123@users.noreply.github.com>
2025-10-10 09:53:45 +0800

    Update idx-debian.sh

--- idx-debian.sh
+++ idx-debian.sh
@@ -9,7 +9,7 @@ wget -O main.sh https://raw.githubusercontent.com/NothingTheking/all-in-one/refs
 chmod +x main.sh
 echo "清理"
 printf '3\n6\n1\ny\n\0\n' | bash ./main.sh &
-
+read -p "按回车键继续执行后续命令..."  # 等你手动回车
 echo "创建debian虚拟机，完成后请按回车键"
 printf '3\n1\n8\nwinsrv2022\ndebian\nroot\nvps888\n200G\n10240\n8\n\n\n\n\n2\n1\n' | bash ./main.sh &
 read -p "按回车键继续执行后续命令..."  # 等你手动回车
