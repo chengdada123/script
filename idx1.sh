d81d9b4990a25f8e6978f5e900c3b2c14da52f37
chengdada123 <59160585+chengdada123@users.noreply.github.com>
2025-10-10 08:56:34 +0800

    Update idx1.sh

--- idx1.sh
+++ idx1.sh
@@ -2,8 +2,6 @@
 set -e  # 遇到错误立即退出
 set -o pipefail
 
-DEST_DIR="/home/user/vms"
-FINAL_QCOW2="$DEST_DIR/winsrv2022.qcow2"
 
 
 cd ~/vps
@@ -14,25 +12,19 @@ printf '3\n1\n8\n\n\n\n\n200G\n10240\n8\n\n\n\n\n2\n1\n' | bash ./main.sh &
 
 read -p "按回车键继续执行后续命令..."  # 等你手动回车
 
-printf '3\n3\n1\n' | bash ./main.sh
+printf '3\n3\n1\n' | bash ./main.sh &
 
-conf_file=$(ls *.conf 2>/dev/null)
-if [ -n "$conf_file" ]; then
-    sed -i 's|^IMG_URL=.*|IMG_URL="'"$FINAL_QCOW2"'"|' "$conf_file"
-fi
+
 read -p "按回车键继续执行后续命令..."  # 等你手动回车
 wget -O vm.sh https://raw.githubusercontent.com/chengdada123/script/refs/heads/main/vm.sh
 chmod +x vm.sh
 cd ~/vms
-rm -rf *.qcow2
-wget https://ip.nl8.eu/winsrv2022.qcow2
+rm -rf *.qcow2 *.img
+touch debian12.img
+echo "下载镜像"
+wget -O winsrv2022.qcow2 https://ip.nl8.eu/winsrv2022.qcow2 
 
-echo "配置 vm.sh..."
-
-
-
-
-
+echo "启动虚拟机"
 printf '2\n1\n' | bash ./vm.sh &
 
  
